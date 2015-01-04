e164add014e5c389cd8fb071f9a33ff20ae6d1d5
George Fleming <gflemingiii@gmail.com>
2015-01-04 18:18:06 +0000

    With hidden snippets

--- snippets_app/snippets.py
+++ snippets_app/snippets.py
@@ -11,48 +11,48 @@ logging.debug("Database connection established.")
 
 
 
-def put(name, snippet):
+def put(name, snippet, hide, unhide, no_hide, show):
+    
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
-    cursor = connection.cursor()
     with connection,connection.cursor() as cursor: 
-        try:
-            cursor.execute( "insert into snippets values (%s, %s)", (name, snippet))
-        except psycopg2.IntegrityError as e:
-            connection.rollback()
+        cursor.execute("select * from snippets where keyword=%s", (name,))
+        row_exists = cursor.fetchone()
+        if row_exists:
             cursor.execute("update snippets set message=%s where keyword=%s", (snippet, name))
-    connection.commit()
+        else:
+            cursor.execute( "insert into snippets values (%s, %s)", (name, snippet))
     logging.debug("Snippet stored successfully.")
-    return name, snippet
+    return name, snippet, hide, unhide, no_hide, show
     
 def catalog():
     logging.info("Retrieving keywords")
     cursor = connection.cursor()
-    cursor.execute( "select * from snippets order by keyword", )
+    cursor.execute( "select * from snippets where hidden=false order by keyword", )
     keys = cursor.fetchall()
     connection.commit()
     print keys
-    
+   
+
       
 def search(word):
     logging.info("Searching for word in snippet")
     cursor = connection.cursor()
-    command = "select * from snippets where message like %s"
-    sure = ['%' + word +'%']
-    cursor.execute(command, sure,)
+    command = "select * from snippets where message like %s and hidden=false"
+    sure = '%' + word + '%'
+    cursor.execute(command, (sure,))
     words = cursor.fetchall()
     connection.commit()
     print words
-
+    
 def get(name):
     logging.info("Retrieving snippet {!r}".format(name))
     with connection, connection.cursor() as cursor:
-        cursor.execute("select message from snippets where keyword=%s", (name,))
+        cursor.execute("select message from snippets where keyword=%s and hidden=False", (name,))
         row = cursor.fetchone()
-    if row:
         return row[0]
-    else:
-        print "Sorry that snippet does not exist."
-    
+   
+
+
 
 def main():
     """Main function"""
@@ -66,7 +66,10 @@ def main():
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="The name of the snippet")
     put_parser.add_argument("snippet", help="The snippet text")
-    
+    put_parser.add_argument("--hide", help="Hide snippet from search and catalog", default=False, action="store_true")
+    put_parser.add_argument("--unhide", help="Unhide snippet from search and catalog", default=True, action="store_false")
+    put_parser.add_argument("--no_hide", help="Unhide all snippets from search and catalog", default=True, action="store_false")
+    put_parser.add_argument("--show", help="Show hidden snippets", default=False, action="store_true")
     logging.debug("Constructing get subparser")
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="The name of the snippet")
@@ -80,16 +83,43 @@ def main():
     
     arguments = parser.parse_args(sys.argv[1:])
     
+    
     arguments = vars(arguments)
+            
     command = arguments.pop("command")
 
+    
+    
     if command == "put":
-        name, snippet = put(**arguments)
-        print("Stored {!r} as {!r}".format(snippet, name)) 
+        name, snippet, hide, unhide, no_hide, show = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name,)) 
         
+        if arguments["hide"]==True:
+            with connection,connection.cursor() as cursor:
+                cursor.execute("update snippets set hidden=True where keyword=%s",(name,))
+                connection.commit()
+                
+        elif arguments["show"]==True:
+            with connection, connection.cursor() as cursor:
+                cursor.execute("select * from snippets where hidden=True")
+                hides = cursor.fetchall()
+                connection.commit()
+                print hides
+                
+        elif arguments["unhide"] == False:
+            with connection,connection.cursor() as cursor:
+                cursor.execute("update snippets set hidden=False where keyword=%s",(name,))
+                connection.commit()
+
+        elif arguments["no_hide"] == False:
+            with connection,connection.cursor() as cursor:
+                cursor.execute("update snippets set hidden=False",)
+                connection.commit()
+        
+                      
+                        
     elif command == "catalog":
-        it = catalog()
-        return it
+        catalog()
     
     elif command == "search":
         word = search(**arguments)
@@ -98,6 +128,8 @@ def main():
     elif command == "get":
         snippet = get(**arguments)
         print("Retrieved snippet: {!r}".format(snippet))
-
+        
+            
 if __name__ == "__main__":
     main()
+    connection.close()
